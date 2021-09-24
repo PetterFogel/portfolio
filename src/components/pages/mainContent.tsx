8fa552bc560a5ea13a08764eb6c434d5b85a61f7
Petter <petterfogel96@gmail.com>
2021-09-24 18:03:12 +0200

    Add skills and project components in main.

--- src/components/pages/mainContent.tsx
+++ src/components/pages/mainContent.tsx
@@ -1,12 +1,16 @@
 import classes from "../../styles/mainContent.module.css";
 import About from "../main/about";
 import Intro from "../main/intro";
+import Projects from "../main/Projects";
+import Skills from "../main/skills";
 
 function MainContent() {
   return (
     <main className={classes.main}>
       <Intro />
       <About />
+      <Skills />
+      <Projects />  
     </main>
   );
 }
