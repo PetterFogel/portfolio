f1fa97d3e9192736e150852f0f43aaccf57b929d
Petter <petterfogel96@gmail.com>
2021-09-27 19:30:57 +0200

    Add a footer with paragraph.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Fragment } from 'react';
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import MainContent from './components/pages/mainContent';
+import classes from './styles/global.module.css';
 
-function App() {
+function App() {  
   return (
     <Fragment>
       <Navigation />
       <MainContent />
+      <footer className={classes.footer}>
+        Created by me, Petter Fogel
+      </footer>
     </Fragment>
   );
 }
