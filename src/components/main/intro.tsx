c9d8eac3e6a7804b2abfe13598d14e80a473c657
Petter <petterfogel96@gmail.com>
2021-10-05 19:41:21 +0200

    Adjusted h2 elements for heading.

--- src/components/main/intro.tsx
+++ src/components/main/intro.tsx
@@ -20,11 +20,11 @@ function IntroContent() {
   return (
     <section className={classes.section}>
       <animated.div style={fadeIn}>
-        <h2 className={classes.subHeadTitle}>
-          Hello, World!
-          <h1 className={classes.headTitle}>My name is Petter Fogel.</h1>I am a
-          Front-End developer student with passion for creative solutions and
-          design.
+        <h2 className={classes.subHeadTitle}>Hello, World!</h2>
+        <h1 className={classes.headTitle}>My name is Petter Fogel.</h1>
+        <h2>
+          I am a Front-End developer student with passion for creative solutions
+          and design.
         </h2>
       </animated.div>
       <div className={classes.arrowHolder}>
