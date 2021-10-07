3e278e91c9cdd804926660d492b534253c245d77
Petter <petterfogel96@gmail.com>
2021-10-07 20:14:58 +0200

    Add github repo link.

--- src/components/main/Projects.tsx
+++ src/components/main/Projects.tsx
@@ -1,18 +1,28 @@
 import classes from "../../styles/global.module.css";
 
 function Projects() {
-    return(
-        <section className={classes.section}>
-            <h2 className={classes.sectionTitle}>What I've done</h2>
-            <p className={classes.paragraph}>All my projects can be viewed at my github.</p>
-            <div className={classes.imageContainer}>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </section>
-    );
+  return (
+    <section className={classes.section}>
+      <h2 className={classes.sectionTitle}>What I've done</h2>
+      <div className={classes.flexRow}>
+        <p className={classes.paragraph}>All my projects can be viewed at my&nbsp;</p>
+        <a
+          className={classes.a}
+          href="https://github.com/PetterFogel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github.
+        </a>
+      </div>
+      <div className={classes.imageContainer}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
