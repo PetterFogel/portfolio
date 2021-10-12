2015e9e53122d035de8cde0eb995db98599b6689
Petter <petterfogel96@gmail.com>
2021-10-12 19:33:31 +0200

    Import new images to render.

--- src/components/main/Projects.tsx
+++ src/components/main/Projects.tsx
@@ -1,12 +1,16 @@
 import { FC } from "react";
 import classes from "../../styles/global.module.css";
+import movieBox from "../../assets/movie-box.png";
+import shoeway from "../../assets/shoeway.png";
 
 const Projects: FC = () => {
   return (
     <section className={classes.section}>
       <h2 className={classes.sectionTitle}>What I've done</h2>
       <div className={classes.flexRow}>
-        <p className={classes.paragraph}>All my projects can be viewed at my&nbsp;</p>
+        <p className={classes.paragraph}>
+          All my projects can be viewed at my&nbsp;
+        </p>
         <a
           className={classes.a}
           href="https://github.com/PetterFogel"
@@ -17,13 +21,21 @@ const Projects: FC = () => {
         </a>
       </div>
       <div className={classes.imageContainer}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className={classes.imageHolder}>
+          <img className={classes.image} src={movieBox} alt="movie-box" />
+        </div>
+        <div>
+          <img className={classes.image} src={shoeway} alt="movie-box" />
+        </div>
+        <div>
+          <img className={classes.image} src={movieBox} alt="movie-box" />
+        </div>
+        <div>
+          <img className={classes.image} src={movieBox} alt="movie-box" />
+        </div>
       </div>
     </section>
   );
-}
+};
 
 export default Projects;
