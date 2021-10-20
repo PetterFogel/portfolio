0d83e770971afdfdfba66e0ff7ca7908b606499d
Petter <petterfogel96@gmail.com>
2021-10-20 19:17:11 +0200

    Add classes for animation when clicking burger-menu.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,13 +1,23 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import classes from "../../styles/navigation.module.css";
 
 const Navigation: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const clickHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className={classes.header}>
       <h2 className={classes.logo}>Petter</h2>
-      <div className={classes.burgerMenu}>
-        <div className={classes.burgerBar}></div>
-        <div className={classes.burgerBar}></div>
+      <div className={classes.burgerMenu} onClick={clickHandler}>
+        <div
+          className={isOpen ? classes.burgerBar1Rotate : classes.burgerBar1}
+        ></div>
+        <div
+          className={isOpen ? classes.burgerBar2Rotate : classes.burgerBar2}
+        ></div>
       </div>
       <div className={classes.fixedEmail}>
         <a className={classes.aLink} href="mailto:petterfogel96@gmail.com">
