e9cc80093d4cb448357a136ef68823941f794da2
Petter <petterfogel96@gmail.com>
2021-10-21 19:29:32 +0200

    Add state to display modal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { FC, Fragment } from 'react';
-import './App.css';
-import Navigation from './components/navigation/navigation';
-import MainContent from './components/pages/mainContent';
-import classes from './styles/global.module.css';
+import { FC, Fragment, useState } from "react";
+import "./App.css";
+import MenuModal from "./components/navigation/menuModal";
+import Navigation from "./components/navigation/navigation";
+import MainContent from "./components/pages/mainContent";
+import classes from "./styles/global.module.css";
 
-const App: FC = () => {  
+const App: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const clickHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Fragment>
-      <Navigation />
+      <Navigation onBurgerMenuClick={clickHandler} />
+      <MenuModal isOpen={isOpen} onCloseModalClick={clickHandler} />
       <MainContent />
-      <footer className={classes.footer}>
-        Created by me, Petter Fogel
-      </footer>
+      <footer className={classes.footer}>Created by me, Petter Fogel</footer>
     </Fragment>
   );
-}
+};
 
 export default App;
