d9a838efc7bd8dac9eb1b44b2166633426a6d3da
PetterFogel <petterfogel96@gmail.com>
2022-03-05 12:51:36 +0100

    Removed animation for project section + some styling

--- src/components/main/projects/projects.tsx
+++ src/components/main/projects/projects.tsx
@@ -2,54 +2,32 @@ import { FC } from "react";
 import classes from "../../../styles/global.module.css";
 import ProjectItem from "./projectItem";
 import { ProjectListData } from "../../../data/projectListData";
-import { useAnimation, motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
 
 const Projects: FC = () => {
-  const animation = useAnimation();
-  const [ref, inView] = useInView({
-    threshold: 0.8,
-  });
-
-  if (inView) {
-    animation.start({
-      opacity: 1,
-      y: 0,
-      x: 0,
-      transition: {
-        type: "spring",
-        duration: 3,
-      },
-    });
-  }
-
   return (
-    <section ref={ref} className={classes.section} id="projects">
-      <motion.div animate={animation} initial={{ opacity: 0, x: 100 }}>
-        <h2 className={classes.sectionTitle}>What I've done</h2>
-      </motion.div>
-      <motion.div animate={animation} initial={{ opacity: 0, y: 100 }}>
-        <div>
-          <div className={classes.flexRow}>
-            <p className={classes.paragraph}>
-              Some of my recent work. All my projects can be viewed at my&nbsp;
-              <a
-                className={classes.githubLink}
-                href="https://github.com/PetterFogel"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                github.
-              </a>
-            </p>
-          </div>
-          <div className={classes.imageContainer}>
-            {ProjectListData.map((project, index) => (
-              <ProjectItem key={index} project={project} />
-            ))}
-          </div>
+    <section className={classes.section} id="projects">
+      <h2 className={classes.sectionTitle}>What I've done</h2>
+
+      <div>
+        <div className={classes.flexRow}>
+          <p className={classes.paragraph}>
+            Some of my recent work. All my projects can be viewed at my&nbsp;
+            <a
+              className={classes.githubLink}
+              href="https://github.com/PetterFogel"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              github.
+            </a>
+          </p>
         </div>
-      </motion.div>
+        <div className={classes.imageContainer}>
+          {ProjectListData.map((project, index) => (
+            <ProjectItem key={index} project={project} />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
