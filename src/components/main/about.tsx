bb721c45cefafc736d3899722328f32abf98a6bb
PetterFogel <petterfogel96@gmail.com>
2022-03-06 18:36:33 +0100

    updated background text.

--- src/components/main/about.tsx
+++ src/components/main/about.tsx
@@ -30,8 +30,8 @@ const About: FC = () => {
         <div>
           <p className={classes.paragraph}>
             I’m a 25 year old based in Gothenburg Sweden. In september 2020, I
-            started my education at Medieinstitutet to pursuit a career as a
-            frontend developer.
+            began the Front End Developer course at Medieinstitutet to pursuit a
+            career as a software developer.
           </p>
           <p className={classes.paragraph}>
             I’v always been creative but I never really knew how to use these
